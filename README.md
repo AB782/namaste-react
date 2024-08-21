7007e4a1c4eb0dc86a37299ec0e011d1f5e39e41
AB782 <aayushbansal782@gmail.com>
2024-08-22 03:06:23 +0530

    useState Hook and distributed App.js in files

--- README.md
+++ README.md
@@ -14,8 +14,44 @@
 - Consistent Hashing
 - Code Splitting
 - differential Bundling - support old browsers
-- Diagnostics 
+- Diagnostics
 - Error Handling
 - HTTPs
 - Tree Shaking - remove unused code
 - different dev and prod bundles
+
+...
+#Namaste Food
+
+/\_-
+1.HEADER
+
+- Logo
+- Nav Items
+  2.BODY
+- Search
+- Restaurant Container - Restaurant Card - Image - Name of res, Rating, cuisine, delivery time 3. FOOTER
+- Copyright
+- Links
+- Address
+- Contact
+  \_/
+
+Two types of Export/Import
+
+1. Default Export/Import
+
+export default Components;
+import Components from "path";
+
+2. Named Export/Import
+
+export const Component
+import {Components} from "path";
+
+# React Hooks
+
+(Normal JS functions ) - written by Facebook developers
+
+- useState() - Superpowerful React Variables
+- useEffect() -
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,68 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-/*-
-  1.HEADER
-      -Logo
-      -Nav Items
-  2.BODY
-     -Search
-     -Restaurant Container
-          - Restaurant Card
-            - Image
-            - Name of res, Rating, cuisine, delivery time
-  3. FOOTER
-      -Copyright
-      -Links
-      -Address
-      -Contact
- */
-
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-// CONFIG - DRIVEN UI
-const RestaurantCard = (props) => {
-  const { resData } = props;
-  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla } =
-    resData?.info;
-  return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-logo"
-        alt="res-logo"
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" +
-          cloudinaryImageId
-        }
-      />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{sla.slaString}</h4>
-    </div>
-  );
-};
-
-const resList = [
+let resList = [
   {
     info: {
       id: "68533",
@@ -1576,28 +1512,4 @@ const resList = [
   },
 ];
 
-const Body = () => {
-  return (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header />
-      <Body />
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<AppLayout />);
+export default resList;
