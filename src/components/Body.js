c28126a6724cda68b679ad4633a3a6bec2724468
AB782 <aayushbansal782@gmail.com>
2024-08-23 16:55:46 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,13 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   // State Variable - Super Powerful variable
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  // Whenever State Variable updates, react triggers a
+  // reconciliation cycle(re - renders the component)
+  console.log("Body renders");
 
   useEffect(() => {
     fetchData();
@@ -21,6 +28,9 @@ const Body = () => {
     setListOfRestaurants(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
   // Conditional Rendering - rendering on the basis of condition
   // if (listOfRestaurants.length === 0) {
@@ -32,6 +42,31 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // Filter the restaurants and update the UI
+              // search Text
+              console.log(searchText);
+              const filteredRestaurant = listOfRestaurants.filter((res) => {
+                return res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -46,7 +81,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
